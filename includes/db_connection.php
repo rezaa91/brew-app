34ce19d759b07cfa3b85db9c32afdd49b06aa716
Rezaa91 <rezaa91@hotmail.co.uk>
2018-04-16 21:08:10 +0100

    provided instructions for database connectivity

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -2,11 +2,14 @@
 
 #connect to 'tea' database
 
+#CHANGE THESE SETTINGS TO YOUR SERVER SETTINGS
+
+
 DEFINE('HOSTNAME','localhost');
 DEFINE('USERNAME','root');
 DEFINE('PASSWORD','');
 DEFINE('DB_NAME','tea');
 
-$dbc = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DB_NAME) or die('count not connect to database '.mysqli_connect_error());
+$dbc = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DB_NAME) or die('could not connect to database '.mysqli_connect_error());
 
 ?>
